039be3cb56ed2a1ff23bddd3575551e76e4221bf
soraamii <thfkazoa@gmail.com>
2023-03-26 22:32:27 +0900

    20230326
    20230326

---
+++ Practice/src/review/Break.java
@@ -0,0 +1,27 @@
+package review;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int i = 0;
+		
+		while(true) {
+			if(sum>100)
+			break;
+			
+			++i;
+			sum += i;
+			
+//			System.out.println("i = " + i);
+//			System.out.println("sum = " + sum);
+				
+		}
+		
+	System.out.println("i = " + i);
+	System.out.println("sum = " + sum);
+
+	}
+
+}
---
+++ Practice/src/review/Byte.java
@@ -0,0 +1,17 @@
+package review;
+
+public class Byte {
+
+	public static void main(String[] args) {
+
+		
+		byte num1 = 10;
+		byte num2 = 20;
+
+		byte num3 = (byte)(num1 + num2);
+		
+		
+
+	}
+
+}
